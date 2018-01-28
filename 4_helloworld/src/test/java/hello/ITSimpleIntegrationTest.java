816a7dc1cb2bc4ddfc4ce70add80594f3fcddc7c
Roland Häusler <rha@ipt.ch>
2018-01-28 14:19:35 +0100

    add gitignore

---
+++ 4_helloworld/src/test/java/hello/ITSimpleIntegrationTest.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.junit.Test;
+
+import com.consol.citrus.junit.AbstractJUnit4CitrusTest;
+
+/**
+ * Integration Test
+ *
+ * @author rha
+ * @since 2018-01-28
+ */
+public class ITSimpleIntegrationTest extends AbstractJUnit4CitrusTest {
+    @Test
+    public void integrationTest() {
+        //executeTest();
+    }
+}
